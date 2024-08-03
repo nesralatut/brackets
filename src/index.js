7fd1de897dce012c7dfef58a3c454841e9388226
Nina Porolonova <127274141+nesralatut@users.noreply.github.com>
2024-08-03 10:21:26 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const openBrackets = new Set();
+  const closeBrackets = new Map();
+
+  for (const [open, close] of bracketsConfig) {
+    openBrackets.add(open);
+    closeBrackets.set(close, open);
+  }
+
+  for (const char of str) {
+    if (openBrackets.has(char)) {
+      if (
+        closeBrackets.get(char) === char &&
+        stack[stack.length - 1] === char
+      ) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    } else if (closeBrackets.has(char)) {
+      if (stack.length === 0 || stack.pop() !== closeBrackets.get(char)) {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
+};
